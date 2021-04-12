e4a6392e688db312d735872ec1cf892f82321ed5
949361487sh <949361487@qq.com>
2021-04-12 16:52:36 +0800

    提交sql

---
+++ sql/member.sql
@@ -0,0 +1,2 @@
+INSERT INTO `member`(`card`, `time`, `name`, `sex`, `age`, `tel`, `integral`, `balance`) VALUES (110, '2021-3-34', '小胖', '1', '18', '13301685003', '90', '120');
+INSERT INTO `member`(`card`, `time`, `name`, `sex`, `age`, `tel`, `integral`, `balance`) VALUES (220, '', '无敌', '2', '29', '18355017897', '188', '200');
---
+++ sql/user.sql
@@ -0,0 +1,2 @@
+INSERT INTO `user`(`id`, `user`, `password`, `name`) VALUES (1, 'admin', '123456', '管理员');
+INSERT INTO `user`(`id`, `user`, `password`, `name`) VALUES (2, 'yuqing', '123456', '雨晴');
